0c0f23b3f114a9b70b72bf38162b63c76c168d9d
Abhinandan Padhi <abhinandan.padhi.123@gmail.com>
2021-08-25 23:40:26 +0800

    Update repo

--- Code/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://atrik123:atrik123@cluster0.gfkos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-  };
--- Code/python_server/popularTimesAPI.py
+++
@@ -1,6 +0,0 @@
-import populartimes
-
-def get_waiting_times(place_id):
-    response = populartimes.get_id("AIzaSyA-XRcHLWd3GVfU0RE6XpbRn86XXG4SsEI", place_id)
-    print(response) 
-    return response
